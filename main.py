7719a7bd42627cc206312959f497616dda44025a
Alejo <aleedin14@gmail.com>
2026-06-25 20:41:03 -0300

    funciones

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #Lista estudiantes
 estudiantes = [
     {"nombre": "Juan", "nota1": 8, "nota2": 7},
-    {"nombre": "Ana", "nota1": 5, "nota2": 6},
+    {"nombre": "Ana", "nota1": 5, "nota2": 5},
     {"nombre": "Luis", "nota1": 3, "nota2": 4}
 ]
 
@@ -26,13 +26,36 @@ while opcion != 4:
         continue
 
     if opcion == 1:
-        pass
+        for estudiante in estudiantes:
+            print(f"Nombre: {estudiante['nombre']}, Nota 1: {estudiante['nota1']}, Nota 2: {estudiante['nota2']}")
 
     elif opcion == 2:
-        pass
+        nombre = input("Ingrese el nombre del estudiante: ")
+        encontrado = False
+        for estudiante in estudiantes:
+            if estudiante["nombre"].lower() == nombre.lower():
+                print(f"Notas: {estudiante['nota1']}, {estudiante['nota2']}")
+                condicion = (estudiante["nota1"] + estudiante["nota2"]) / 2 
+                if condicion >= 7:
+                    print("Condición: Aprobado")
+                else:
+                    print("Condición: Reprobado")
+                encontrado = True
+                break
+        if not encontrado:
+            print("El estudiante no se encuentra en la lista.")
 
     elif opcion == 3:
-        pass
-
+        nombre = input("Ingrese el nombre del estudiante: ")
+        encontrado = False
+        for estudiante in estudiantes:
+            if estudiante["nombre"].lower() == nombre.lower():
+                promedio = (estudiante["nota1"] + estudiante["nota2"]) / 2
+                print(f"El promedio de {estudiante['nombre']} es: {promedio}")
+                encontrado = True
+                break
+        if not encontrado:
+            print("El estudiante no se encuentra en la lista.")
     elif opcion == 4:
         print("Saliendo del programa.")
+        
